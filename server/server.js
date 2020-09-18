4440b7ffae7545a6db90b64e17e763795139e027
LordRegis22 <45404541+LordRegis22@users.noreply.github.com>
2020-09-18 14:54:19 -0700

    add basic server architecture

---
+++ server/server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const pool = require('./models');
+const path = require('path');
+
+// port number
+const PORT = 2222;
+const cors = require('cors');
+const { urlencoded } = require('body-parser');
+
+// prevent cors errors
+app.use(cors());
+// require all interactions to use/parse JSON
+app.use(express.json());
+// handle form data correctly
+app.use(express.urlencoded({ extended: true }));
+
+app.use('client', express.static('client'));
+//app.get('/', (req, res) =>
+//   res.status(200).sendFile(path.resolve(__dirname, '../client/index.html'))
+// );
+// app.post(//when we post, take in endpoint, middleware, cb)
+// app.get(//when we get...)
+// app.put(//when we put...)
+// app.delete(//when we delete)
+
+app.listen(PORT, () => {
+  console.log('yall listening on 2222');
+});
