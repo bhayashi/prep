02c56ad51b1a917b79e206ed05006e2425632da9
b-hash <brianmhayashi@gmail.com>
2020-09-18 14:01:33 -0700

    added models.js

---
+++ server/models.js
@@ -0,0 +1,8 @@
+const { Pool } = require('pg');
+const PG_URL = 'postgres://iqzgopty:YKIPob3fxF_3yPa2ioTXf35wl83pUV08@lallah.db.elephantsql.com:5432/iqzgopty';
+
+const pool = new Pool({ connectString: PG_URL });
+
+module.exports = {
+  query: (text, params, cb) => pool.query(text, params, cb),
+}
