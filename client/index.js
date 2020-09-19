222651482bcdd7a33dddef28635e43ef8e0f849f
b-hash <brianmhayashi@gmail.com>
2020-09-18 22:24:23 -0700

    added more js

--- client/index.js
+++ client/index.js
@@ -1,15 +1,17 @@
+/* eslint-disable no-restricted-syntax */
+/* eslint-disable guard-for-in */
 const body = document.querySelector('body');
 const form = document.createElement('div');
 form.innerHTML = `
 <form action="/api" method="post">
   <label for="item-input">Item: </label>
-  <input type="text"></input>
+  <input id="item" type="text"></input>
   <label for="description-input">Description: </label>
-  <input type="text"></input>
+  <input id="description" type="text"></input>
   <label for="date-input">Date: </label>
-  <input type="text"></input>
+  <input id="date" type="text"></input>
   <label for="checkbox">Completed: </label>
-  <input type="checkbox" id="completed-check" value="completed"></input>
+  <input type="checkbox" id="completed" value="completed"></input>
   <button type="button" id="post-button">Post it!</button>
 </form>
 `;
@@ -17,25 +19,27 @@ body.appendChild(form);
 
 const formItems = ['item', 'description', 'date', 'completed'];
 
+const deleteFunc = (obj_id) => {
+  fetch('/api', {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      id: obj_id,
+    }),
+  })
+    .then((res) => res.json())
+    .then((data) => console.log('deleted', data))
+    .catch((err) => console.log(err));
+};
+
 const getButton = document.createElement('button');
 getButton.innerText = 'GET TODO LIST';
 getButton.type = 'button';
+body.appendChild(getButton);
 getButton.addEventListener('click', () => {
   fetch('/api')
-    .then((res) => res.json())
-    .then((data) => console.log(data))
-    .catch((err) => console.log(err));
-});
-body.appendChild(getButton);
-
-document.getElementById('post-button').onclick = () => {
-  fetch('/api', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({
-      //
-    }),
-  })
     .then((res) => res.json())
     .then((data) => {
       for (const obj of data) {
@@ -44,13 +48,39 @@ document.getElementById('post-button').onclick = () => {
           const innerP = document.createElement('p');
           innerP.innerText = obj[key];
           posts.appendChild(innerP);
-        };
+        }
+        const deleteButton = document.createElement('button');
+        deleteButton.innerText = 'Delete Me!';
+        // deleteButton.addEventListener('click', deleteFunc(obj._id));
+        posts.appendChild(deleteButton);
         body.appendChild(posts);
-      };
+      }
+      console.log(data);
+    })
+    .catch((err) => console.log(err));
+});
+
+const item = document.getElementById('item');
+const description = document.getElementById('description');
+const date = document.getElementById('date');
+
+document.getElementById('post-button').addEventListener('click', () => {
+  fetch('/api', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      item: item.value,
+      description: description.value,
+      date: date.value,
+      status: 'completed',
+    }),
+  })
+    .then((res) => res.json())
+    .then((data) => {
       console.log('Post from index.js', data);
     })
     .catch((err) => console.log('Error in Post from index.js', err));
-};
+});
 
 /*
 element.setAttribute
